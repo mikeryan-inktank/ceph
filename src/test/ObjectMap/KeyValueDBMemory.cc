809c2ba8d6d98994537b2472fc67a548872640c0
Joao Eduardo Luis <joao.luis@inktank.com>
2012-07-13 12:31:43 +0100

    test: KeyValueDBMemory: implement whole-space and read-only iterators
    Following the changes in KeyValueDB, this patch removes the in-memory,
    prefix-dependent iterator and make it a whole-space iterator instead. We
    also add a read-only iterator based on the said whole-space iterator.
    
    Signed-off-by: Joao Eduardo Luis <joao.luis@inktank.com>

--- src/test/ObjectMap/KeyValueDBMemory.cc
+++ src/test/ObjectMap/KeyValueDBMemory.cc
@@ -1,4 +1,5 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
 #include "include/encoding.h"
 #include "KeyValueDBMemory.h"
 #include <map>
@@ -8,91 +9,140 @@
 
 using namespace std;
 
-class MemIterator : public KeyValueDB::IteratorImpl {
-  string prefix;
+class WholeSpaceMemIterator : public KeyValueDB::WholeSpaceIteratorImpl {
+protected:
   KeyValueDBMemory *db;
-
   bool ready;
-  map<string, bufferlist>::iterator iter;
+
+  map<string, map<string, bufferlist> >::iterator db_iter;
+  map<string, bufferlist>::iterator curr_iter;
 
 public:
-  MemIterator(const string &prefix,
-	      KeyValueDBMemory *db) :
-    prefix(prefix), db(db), ready(false) {}
+  WholeSpaceMemIterator(KeyValueDBMemory *db) : db(db), ready(false) { }
+  virtual ~WholeSpaceMemIterator() { }
 
   int seek_to_first() {
-    if (!db->db.count(prefix)) {
+    if (db->db.size() == 0) {
       ready = false;
       return 0;
     }
-    iter = db->db[prefix].begin();
+    db_iter = db->db.begin();
+    curr_iter = (*db_iter).second.begin();
     ready = true;
+    return 0;
+  }
+
+  int seek_to_first(const string &prefix) {
+    if ((db->db.size() == 0) || (db->db.count(prefix) == 0)) {
+      ready = false;
+      return 0;
+    }
+    db_iter = db->db.find(prefix);
+    curr_iter = (*db_iter).second.begin();
+    assert(curr_iter == db->db[prefix].begin());
     return 0;
   }
 
   int seek_to_last() {
-    if (!db->db.count(prefix)) {
+    if (db->db.size() == 0) {
       ready = false;
       return 0;
-    } else if (db->db[prefix].size() == 0) {
-      iter = db->db[prefix].end();
-    } else {
-      iter = --db->db[prefix].end();
     }
+    db_iter = --db->db.end();
+    curr_iter = --(*db_iter).second.end();
     ready = true;
     return 0;
   }
 
-  int lower_bound(const string &to) {
-    if (!db->db.count(prefix)) {
+  int seek_to_last(const string &prefix) {
+    if ((db->db.size() == 0) || (db->db.count(prefix) == 0)) {
       ready = false;
       return 0;
     }
-    iter = db->db[prefix].lower_bound(to);
+    db_iter = db->db.find(prefix);
+    curr_iter = --(*db_iter).second.end();
+    return 0;
+  }
+
+  int lower_bound(const string &prefix, const string &to) {
+    if ((db->db.size() == 0) || (db->db.count(prefix) == 0)) {
+      ready = false;
+      return 0;
+    }
+    db_iter = db->db.find(prefix);
+    curr_iter = (*db_iter).second.lower_bound(to);
     ready = true;
     return 0;
   }
 
-  int upper_bound(const string &after) {
-    if (!db->db.count(prefix)) {
+  int upper_bound(const string &prefix, const string &after) {
+    if ((db->db.size() == 0) || (db->db.count(prefix) == 0)) {
       ready = false;
       return 0;
     }
-    iter = db->db[prefix].upper_bound(after);
+    db_iter = db->db.find(prefix);
+    curr_iter = (*db_iter).second.upper_bound(after);
     ready = true;
     return 0;
   }
 
   bool valid() {
-    return ready && iter != db->db[prefix].end();
+    return ready && (db_iter != db->db.end());
   }
 
   bool begin() {
-    return ready && iter == db->db[prefix].begin();
+    return ready && (db_iter == db->db.begin())
+      && (curr_iter == (*db_iter).second.begin());
   }
 
   int prev() {
-    if (valid() && iter != db->db[prefix].begin())
-      iter--;
+    if (!valid())
+      return 0;
+
+    if (db_iter == db->db.begin()) {
+      if (curr_iter == (*db_iter).second.begin())
+	return 0;
+    } else if (curr_iter == (*db_iter).second.begin()) {
+      --db_iter;
+      curr_iter = --(*db_iter).second.end();
+      return 0;
+    }
+
+    --curr_iter;
     return 0;
   }
 
   int next() {
-    if (valid())
-      iter++;
+    if (!valid() || (db_iter == db->db.end()))
+      return 0;
+    if (curr_iter == (*db_iter).second.end()) {
+      ++db_iter;
+      if (db_iter == db->db.end())
+	return 0;
+      curr_iter = (*db_iter).second.begin();
+      return 0;
+    }
+    ++curr_iter;
     return 0;
   }
 
   string key() {
     if (valid())
-      return iter->first;
+      return (*curr_iter).first;
     else
       return "";
   }
 
+  pair<string,string> raw_key() {
+    if (valid())
+      return make_pair((*db_iter).first, (*curr_iter).first);
+    else
+      return make_pair("", "");
+  }
+
   bufferlist value() {
     if (valid())
-      return iter->second;
+      return (*curr_iter).second;
     else
       return bufferlist();
   }
@@ -100,6 +150,7 @@ public:
   int status() {
     return 0;
   }
+
 };
 
 int KeyValueDBMemory::get(const string &prefix,
@@ -172,6 +223,24 @@ int KeyValueDBMemory::rmkeys_by_prefix(const string &prefix) {
   return 0;
 }
 
-KeyValueDB::Iterator KeyValueDBMemory::get_iterator(const string &prefix) {
-  return tr1::shared_ptr<IteratorImpl>(new MemIterator(prefix, this));
+KeyValueDB::WholeSpaceIterator KeyValueDBMemory::_get_iterator() {
+  return std::tr1::shared_ptr<KeyValueDB::WholeSpaceIteratorImpl>(
+    new WholeSpaceMemIterator(this)
+  );
+}
+
+class WholeSpaceReadOnlyMemIterator : public WholeSpaceMemIterator {
+public:
+  WholeSpaceReadOnlyMemIterator(KeyValueDBMemory *db) :
+    WholeSpaceMemIterator(db) { }
+  ~WholeSpaceReadOnlyMemIterator() {
+    delete db;
+  }
+};
+
+KeyValueDB::WholeSpaceIterator KeyValueDBMemory::_get_readonly_iterator() {
+  KeyValueDBMemory *ro_db = new KeyValueDBMemory(this);
+  return std::tr1::shared_ptr<KeyValueDB::WholeSpaceIteratorImpl>(
+    new WholeSpaceReadOnlyMemIterator(ro_db)
+  );
 }
