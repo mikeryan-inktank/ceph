7ea8245b0823c8199678f13ff4dc0c93ea67f524
Joao Eduardo Luis <joao.luis@inktank.com>
2012-07-23 11:47:00 +0100

    os: KeyValueDB: re-implement (prefix) iter in terms of whole-space iter
    In-a-nutshell-version: Create a whole-space iterator interface, and
    implement the already existing, prefix-based iterator in terms of the
    new whole-space iterator;
    
    This patch introduces a significant change on the architecture of
    KeyValueDB's iterator, although its interface remains the same.
    
    Before this patch, KeyValueDB simply defined an interface for a
    prefix-based interface, to be implemented by derivative classes. Being
    constrained by a prefix-based approach to iterate over the store only makes
    sense when we know which prefixes we want to iterate over, but for that we
    must know about the prefixes beforehand. This approach didn't work when one
    wanted to iterate over the whole key space, without any previous awareness
    about the keys and their prefixes.
    
    This patch introduces a new interface for a whole-space iterator, to be
    implemented by derivative classes, which is prefix-independent. We also
    define an abstract function to obtain this iterator, which must also be
    implemented by the derivative class. With this interface in place, we are
    then able to implement a prefix-dependent iterator in terms of the
    whole-space iterator, which will be offered by the KeyValueDB class itself.
    
    Furthermore, we implement these changes on LevelDBStore and KeyValueDBMemory,
    the in-memory mock store, which leads to significant changes on both:
    
      * LevelDBStore
        - Substitute the previously existing LevelDBIteratorImpl, which
          followed a prefix-based iteration, for
          LevelDBWholeSpaceIteratorImpl, which now iterates over the whole
          key space of the store;
    
      * KeyValueDBMemory:
        - Substitute the previously existing MemIterator, which followed a
          prefix-based iteration, for WholeSpaceMemIterator, which now
          iterates over the whole key space of the in-memory mock store;
        - Change the in-memory mock store data structure. Previously, we
          used a map-of-maps, mapping prefixes to a key/value map; now we
          keep a single map, mapping (prefix,key) pairs to values.
    
    Signed-off-by: Joao Eduardo Luis <joao.luis@inktank.com>

--- src/test/ObjectMap/KeyValueDBMemory.cc
+++ src/test/ObjectMap/KeyValueDBMemory.cc
@@ -9,98 +9,148 @@
 
 using namespace std;
 
-class MemIterator : public KeyValueDB::IteratorImpl {
-  string prefix;
+class WholeSpaceMemIterator : public KeyValueDB::WholeSpaceIteratorImpl {
+protected:
   KeyValueDBMemory *db;
-
   bool ready;
-  map<string, bufferlist>::iterator iter;
+
+  map<pair<string,string>, bufferlist>::iterator it;
 
 public:
-  MemIterator(const string &prefix,
-	      KeyValueDBMemory *db) :
-    prefix(prefix), db(db), ready(false) {}
+  WholeSpaceMemIterator(KeyValueDBMemory *db) : db(db), ready(false) { }
+  virtual ~WholeSpaceMemIterator() { }
 
   int seek_to_first() {
-    if (!db->db.count(prefix)) {
+    if (db->db.size() == 0) {
+      it = db->db.end();
       ready = false;
       return 0;
     }
-    iter = db->db[prefix].begin();
+    it = db->db.begin();
+    ready = true;
+    return 0;
+  }
+
+  int seek_to_first(const string &prefix) {
+    it = db->db.lower_bound(make_pair(prefix, ""));
+    if ((db->db.size() == 0) || (it == db->db.end()) ||
+       ((*it).first.first != prefix)) {
+      it = db->db.end();
+      ready = false;
+      return 0;
+    }
+    assert((*it).first.first == prefix);
     ready = true;
     return 0;
   }
 
   int seek_to_last() {
-    if (!db->db.count(prefix)) {
+    it = db->db.end();
+    if (db->db.size() == 0) {
       ready = false;
       return 0;
-    } else if (db->db[prefix].size() == 0) {
-      iter = db->db[prefix].end();
-    } else {
-      iter = --db->db[prefix].end();
     }
+    --it;
+    assert(it != db->db.end());
     ready = true;
     return 0;
   }
 
-  int lower_bound(const string &to) {
-    if (!db->db.count(prefix)) {
+  int seek_to_last(const string &prefix) {
+    string tmp(prefix);
+    tmp.append(1, (char) CHAR_MIN);
+    it = db->db.upper_bound(make_pair(tmp, ""));
+
+    if ((db->db.size() == 0) || (it == db->db.begin()))
+      goto error_end;
+
+    if (it == db->db.end())
+      goto error_out;
+
+    --it;
+
+    if ((*it).first.first != prefix)
+      goto error_end;
+
+    ready = true;
+    return 0;
+
+  error_end:
+    it = db->db.end();
+  error_out:
+    ready = false;
+    return 0;
+  }
+
+  int lower_bound(const string &prefix, const string &to) {
+    it = db->db.lower_bound(make_pair(prefix,to));
+    if ((db->db.size() == 0) || (it == db->db.end()) ||
+	((*it).first.first != prefix)) {
+      it = db->db.end();
       ready = false;
       return 0;
     }
-    iter = db->db[prefix].lower_bound(to);
+
+    assert(it != db->db.end());
+    assert((*it).first.first == prefix);
+
     ready = true;
     return 0;
   }
 
-  int upper_bound(const string &after) {
-    if (!db->db.count(prefix)) {
+  int upper_bound(const string &prefix, const string &after) {
+    it = db->db.upper_bound(make_pair(prefix,after));
+    if ((db->db.size() == 0) || (it == db->db.end()) ||
+	((*it).first.first != prefix)) {
+      it = db->db.end();
       ready = false;
       return 0;
     }
-    iter = db->db[prefix].upper_bound(after);
+    assert(it != db->db.end());
+    assert((*it).first.first == prefix);
     ready = true;
     return 0;
   }
 
   bool valid() {
-    return ready && iter != db->db[prefix].end();
+    return ready && (it != db->db.end());
   }
 
   bool begin() {
-    return ready && iter == db->db[prefix].begin();
+    return ready && (it == db->db.begin());
   }
 
   int prev() {
-    if (valid() && iter != db->db[prefix].begin())
-      iter--;
+    if (!begin() && ready)
+      --it;
+    else
+      it = db->db.end();
     return 0;
   }
 
   int next() {
     if (valid())
-      iter++;
+      ++it;
     return 0;
   }
 
   string key() {
     if (valid())
-      return iter->first;
+      return (*it).first.second;
     else
       return "";
   }
 
   pair<string,string> raw_key() {
     if (valid())
-      return make_pair(prefix, key());
+      return (*it).first;
     else
-      return make_pair("","");
+      return make_pair("", "");
   }
 
   bufferlist value() {
     if (valid())
-      return iter->second;
+      return (*it).second;
     else
       return bufferlist();
   }
@@ -113,14 +163,15 @@ public:
 int KeyValueDBMemory::get(const string &prefix,
 			  const std::set<string> &key,
 			  map<string, bufferlist> *out) {
-  if (!db.count(prefix))
+  if (!exists_prefix(prefix))
     return 0;
 
   for (std::set<string>::const_iterator i = key.begin();
        i != key.end();
        ++i) {
-    if (db[prefix].count(*i))
-      (*out)[*i] = db[prefix][*i];
+    pair<string,string> k(prefix, *i);
+    if (db.count(k))
+      (*out)[*i] = db[k];
   }
   return 0;
 }
@@ -128,13 +179,13 @@ int KeyValueDBMemory::get(const string &prefix,
 int KeyValueDBMemory::get_keys(const string &prefix,
 			       const std::set<string> &key,
 			       std::set<string> *out) {
-  if (!db.count(prefix))
+  if (!exists_prefix(prefix))
     return 0;
 
   for (std::set<string>::const_iterator i = key.begin();
        i != key.end();
        ++i) {
-    if (db[prefix].count(*i))
+    if (db.count(make_pair(prefix, *i)))
       out->insert(*i);
   }
   return 0;
@@ -143,24 +194,35 @@ int KeyValueDBMemory::get_keys(const string &prefix,
 int KeyValueDBMemory::set(const string &prefix,
 			  const string &key,
 			  const bufferlist &bl) {
-  db[prefix][key] = bl;
+  db[make_pair(prefix,key)] = bl;
   return 0;
 }
 
 int KeyValueDBMemory::rmkey(const string &prefix,
 			    const string &key) {
-  db[prefix].erase(key);
-  if (db[prefix].size() == 0)
-    db.erase(prefix);
-
+  db.erase(make_pair(prefix,key));
   return 0;
 }
 
 int KeyValueDBMemory::rmkeys_by_prefix(const string &prefix) {
-  db.erase(prefix);
+  map<std::pair<string,string>,bufferlist>::iterator i;
+  i = db.lower_bound(make_pair(prefix, ""));
+  if (i == db.end())
+    return 0;
+
+  while (i != db.end()) {
+    std::pair<string,string> key = (*i).first;
+    if (key.first != prefix)
+      break;
+
+    ++i;
+    rmkey(key.first, key.second);
+  }
   return 0;
 }
 
-KeyValueDB::Iterator KeyValueDBMemory::get_iterator(const string &prefix) {
-  return tr1::shared_ptr<IteratorImpl>(new MemIterator(prefix, this));
+KeyValueDB::WholeSpaceIterator KeyValueDBMemory::_get_iterator() {
+  return std::tr1::shared_ptr<KeyValueDB::WholeSpaceIteratorImpl>(
+    new WholeSpaceMemIterator(this)
+  );
 }
