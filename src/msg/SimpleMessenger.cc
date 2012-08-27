2228efda60546ff14059be2cf2362e5eaea5827a
Samuel Just <sam.just@inktank.com>
2012-08-27 10:54:00 -0700

    do_sendmsg hack

--- src/msg/SimpleMessenger.cc
+++ src/msg/SimpleMessenger.cc
@@ -2117,7 +2117,7 @@ int SimpleMessenger::Pipe::write_message(Message *m)
 	     << dendl;
     
     if (msg.msg_iovlen >= IOV_MAX-2) {
-      if (do_sendmsg(&msg, msglen, true))
+      if (do_sendmsg(&msg, msglen, false))
 	goto fail;
       
       // and restart the iov
